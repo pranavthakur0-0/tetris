44913e7600ae24f518469677d15aa6ea905e64a7
pranavthakur <pranavthakurdew@gmail.com>
2026-03-02 01:58:31 +0530

    basic

--- main.go
+++ main.go
@@ -5,113 +5,173 @@ import (
 	"time"
 )
 
-
 const (
-    BoardWidth  = 10
-    BoardHeight = 20
+	BoardWidth  = 10
+	BoardHeight = 20
 )
 
 type Piece struct {
-	x int
-	y int
+	x     int
+	y     int
 	shape [][]int
 }
 
-
 func newSquarePiece() Piece {
 	shape := [][]int{
-		{0, 1, 0},
-		{1, 1, 1},
+		{1, 1},
+		{1, 1},
 	}
 	return Piece{
-		x : 4,
-		y : 0,
+		x:     4,
+		y:     0,
 		shape: shape,
 	}
 }
 
-
-
-
-func drawBoard(board [][]int){
+func drawBoard(board [][]int, piece Piece) {
 	for x := 0; x < BoardHeight; x++ {
-		for y := 0; y < BoardWidth; y++{
-			if board[x][y] == 0 {
-                fmt.Print(". ")
-            } else {
-                fmt.Print("# ")
-            }
+		for y := 0; y < BoardWidth; y++ {
+			filled := (board[x][y] == 1)
+			if !filled && pieceAt(x, y, piece) {
+				filled = true
+			}
+			if filled {
+				fmt.Print("# ")
+			} else {
+				fmt.Print(". ")
+			}
 		}
 		fmt.Println()
 	}
 }
 
-func placePieceOnBoard(board [][]int, piece Piece){
+func collapseTheRow(board [][]int, row int) {
+	for y := row; y > 0; y-- {
+		for x := BoardWidth - 1; x >= 0; x-- {
+			board[y][x] = board[y-1][x]
+		}
+	}
+
+	for i := 0; i < BoardWidth; i++ {
+		board[0][i] = 0
+	}
+}
+
+func removeIfLineIsPresent(board [][]int) {
+	for row := BoardHeight - 1; row >= 0; row-- {
+		cleanRow := true
+		for col := BoardWidth - 1; col >= 0; col-- {
+			if board[row][col] != 1 {
+				cleanRow = false
+			}
+		}
+		if cleanRow {
+			collapseTheRow(board, row)
+			row++
+		}
+	}
+}
+
+func pieceAt(row int, col int, piece Piece) bool {
+	py := row - piece.y
+	px := col - piece.x
+	if py < 0 || py >= len(piece.shape) || px < 0 || px >= len(piece.shape[0]) {
+		return false
+	}
+	return piece.shape[py][px] == 1
+}
+
+func placePieceOnBoard(board [][]int, piece Piece) {
 	for py := 0; py < len(piece.shape); py++ {
 		for px := 0; px < len(piece.shape[py]); px++ {
 			if piece.shape[py][px] == 1 {
-                board[piece.y+py][piece.x+px] = 1
-            }
+				board[piece.y+py][piece.x+px] = 1
+			}
 		}
 	}
 }
 
-
-func clearBoard(board [][]int){
+func clearBoard(board [][]int) {
 	for x := 0; x < BoardHeight; x++ {
-		for y := 0; y < BoardWidth; y++{
-			 board[x][y] = 0 
+		for y := 0; y < BoardWidth; y++ {
+			board[x][y] = 0
 		}
 	}
 }
 
-func canMoveSide(piece Piece, dx int) bool {
+func canMoveSide(board [][]int, piece Piece, dx int) bool {
 	newX := piece.x + dx
-	return newX >= 0 && newX+len(piece.shape[0]) <= BoardWidth
+	if newX < 0 || newX+len(piece.shape[0]) > BoardWidth {
+		return false
+	}
+	for py := 0; py < len(piece.shape); py++ {
+		for px := 0; px < len(piece.shape[0]); px++ {
+			if piece.shape[py][px] != 1 {
+				continue
+			}
+			boardRow := piece.y + py
+			boardCol := newX + px
+			if board[boardRow][boardCol] == 1 {
+				return false
+			}
+		}
+	}
+	return true
 }
 
-func canMove(piece Piece) bool {
-	return piece.y+len(piece.shape) < BoardHeight
+func canMove(board [][]int, piece Piece) bool {
+	if piece.y+len(piece.shape) >= BoardHeight {
+		return false
+	}
+	for py := 0; py < len(piece.shape); py++ {
+		for px := 0; px < len(piece.shape[0]); px++ {
+			if piece.shape[py][px] != 1 {
+				continue
+			}
+			boardRowBelow := piece.y + py + 1
+			boardCol := piece.x + px
+			if board[boardRowBelow][boardCol] == 1 {
+				return false
+			}
+		}
+	}
+	return true
 }
-
-
 
 func main() {
 	board := make([][]int, BoardHeight)
-	for i := 0; i < BoardHeight; i++{
-		board[i] = make([]int,BoardWidth)
+	for i := 0; i < BoardHeight; i++ {
+		board[i] = make([]int, BoardWidth)
 	}
-	piece := newSquarePiece();
+
+	piece := newSquarePiece()
 	for {
 		// clearBoard(board)
-		
-		fmt.Print("\033[H\033[2J")
-		placePieceOnBoard(board, piece)
-		drawBoard(board)
 
-		var input string 
+		fmt.Print("\033[H\033[2J")
+		removeIfLineIsPresent(board)
+		drawBoard(board, piece)
+
+		var input string
 		fmt.Scanln(&input)
 
-		if input == "a" && canMoveSide(piece, -1) {
+		if input == "a" && canMoveSide(board, piece, -1) {
 			piece.x--
 		}
-	
-		if input == "d" && canMoveSide(piece, 1) {
+
+		if input == "d" && canMoveSide(board, piece, 1) {
 			piece.x++
 		}
-	
-		if input == "s" && canMove(piece) {
+
+		if input == "s" && canMove(board, piece) {
 			piece.y++
 		}
-		if canMove(piece) {
+		if canMove(board, piece) {
 			piece.y++
-		}else{
+		} else {
+			placePieceOnBoard(board, piece)
 			piece = newSquarePiece()
 		}
-
-		// piece.y++clear
 		time.Sleep(300 * time.Millisecond)
 	}
-	
 }
-
